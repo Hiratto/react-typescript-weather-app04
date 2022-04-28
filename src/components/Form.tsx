91ba1a457397b1b7055dfe968e9917f995607d73
TAKAHISA HIRANO <hiratto@me.com>
2022-04-28 13:29:48 +0900

    FormにPropsを渡す

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,16 @@
+type FormPropsType = {
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+  getWeather: (e: any) => void;
+}
 
-
-
-
-const Form = () => {
-  
+const Form = ( props: FormPropsType ) => {
   return (
     <form>
-      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-      <button type="submit" onClick={getWeather}>Get Weather</button>
+      <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)}/>
+      <button type="submit" onClick={props.getWeather}>GetWeather</button>
     </form>
-  );
-};
+    );
+  };
+  
+  export default Form;
 
-export default Form ;
